dfb2c1cfd93f74d8241d6aa8f87aeb0c97908d55
rajeshkannahg <rajeshkannah.g@syncfusion.com>
2024-03-20 15:34:32 +0530

    tested

--- index.js
+++ index.js
@@ -6,23 +6,18 @@ var worker = new SharedWorker('shared_worker.js');
 // Get the port for communication
 var port = worker.port;
 
-// Listen for messages from the shared worker
-port.onmessage = function(event) {
-  // Log the received message
-  console.log('Message received in main script:1', event.data);
-};
-
 
 
 // Listen for messages from the shared worker
-worker.port.onmessage = function(event) {
-  // Log the received message
-  console.log('Message received in main script:1', event.data);
-};
+worker.port.onmessage = handleMessageFromSharedWorker;
 
+// Function to handle messages received from the shared worker
+function handleMessageFromSharedWorker(event) {
+  console.log('Message received in tab:', event.data);
+}
 function sendRandomMessage() {
   const randomStr = generateRandomString(); // Function to generate random string
-  port.postMessage('Hello from main script 1 ' + randomStr);
+  port.postMessage('Hello from main script ' + randomStr);
 }
 
 function generateRandomString() {
@@ -34,5 +29,3 @@ function generateRandomString() {
   }
   return result;
 }
-
-setInterval(sendRandomMessage, 5000);
--- shared_worker.js
+++ shared_worker.js
@@ -2,29 +2,25 @@
 var workerName = generateRandomString();
 // Listen for connections
 onconnect = function(e) {
-    // Get the port for communication
-    var port = e.ports[0];
+  // Get the port for this connection
+  const port = e.ports[0];
+
+  // Event listener for messages from the main page
+  port.onmessage = function(event) {
+    // Log the received message
+    console.log('Message received in shared worker:', + workerName + event.data);
     
-    // Handle messages from the main script
-    port.onmessage = function(event) {
-      // Log the received message
-      console.log('Message received in shared worker:1', event.data);
-      // Echo the message back to the main script
-      port.postMessage("Echo from" + workerName + event.data);
-      // Send a message to all connected tabs
+    // Broadcast the message to all connected tabs
     self.clients.matchAll().then(clients => {
       clients.forEach(client => {
-        client.postMessage('Message from shared worker: '+ workerName + event.data);
+        client.postMessage(event.data);
       });
     });
   };
 
-    port.start(); // Start listening for messages
-    
-  
-    // Let the main script know that the shared worker is ready
-    port.postMessage('Shared worker' + workerName+  'connected');
-  };
+  // Start listening for messages
+  port.start();
+};
   
   function generateRandomString() {
     const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
